2fad3d1668a8062ab5ebb62dc29969603ad8f43c
Nolan Conaway <nolanbconaway@gmail.com>
2022-12-03 11:25:34 -0500

    extend tests to py310

--- setup.py
+++ setup.py
@@ -42,6 +42,9 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     keywords=["nyc", "transit", "subway", "command-line", "cli"],
     license="MIT",
